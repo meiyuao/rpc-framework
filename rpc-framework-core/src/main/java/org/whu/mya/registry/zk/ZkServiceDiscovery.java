ee4c49f6a1edcd2352b29ccb7c98cf11264f3be0
meiyuao <meiyuao@qq.com>
2020-11-16 10:27:36 +0800

    继续开发了一部分 zookeeper 服务与注册功能

--- rpc-framework-core/src/main/java/org/whu/mya/registry/zk/ZkServiceDiscovery.java
+++ rpc-framework-core/src/main/java/org/whu/mya/registry/zk/ZkServiceDiscovery.java
@@ -1,13 +1,28 @@
 package org.whu.mya.registry.zk;
 
+import org.apache.curator.framework.CuratorFramework;
 import org.whu.mya.registry.ServiceDiscovery;
+import org.whu.mya.registry.zk.util.CuratorUtils;
 
 import java.net.InetSocketAddress;
+import java.util.List;
 
 public class ZkServiceDiscovery implements ServiceDiscovery {
     @Override
     public InetSocketAddress lookupService(String rpcServiceName) {
+        CuratorFramework zkClient = CuratorUtils.getZkClient();
+        List<String> serviceUrlList = CuratorUtils.getChildrenNodes(zkClient, rpcServiceName);
 
-        return null;
+        if (serviceUrlList == null || serviceUrlList.size() == 0) {
+            throw new RuntimeException();
+        }
+
+        // 负载均衡选择服务器
+        String targetServiceUrl = serviceUrlList.get(0);
+
+        String[] socketAddressArray = targetServiceUrl.split(":");
+        String host = socketAddressArray[0];
+        int port = Integer.parseInt(socketAddressArray[1]);
+        return new InetSocketAddress(host, port);
     }
 }
--- rpc-framework-core/src/main/java/org/whu/mya/registry/zk/util/CuratorUtils.java
+++ rpc-framework-core/src/main/java/org/whu/mya/registry/zk/util/CuratorUtils.java
@@ -7,6 +7,8 @@ import org.apache.curator.framework.imps.CuratorFrameworkState;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -15,7 +17,7 @@ public final class CuratorUtils {
     private static final int MAX_RETRIES = 3;
     public static final String ZK_REGISTER_ROOT_PATH = "/my-rpc";
     private static final Set<String> REGISTERED_PATH_SET = ConcurrentHashMap.newKeySet();
-
+    private static final Map<String, List<String>> SERVICE_ADDRESS_MAP = new ConcurrentHashMap<>();
     private static  CuratorFramework zkClient;
     private static String defaultZookeeperAddress = "192.168.200.176:2181";
     private CuratorUtils(){
@@ -49,5 +51,21 @@ public final class CuratorUtils {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<String> getChildrenNodes(CuratorFramework zkClient, String rpcServiceName) {
+        if (SERVICE_ADDRESS_MAP.containsKey(rpcServiceName)) {
+            return SERVICE_ADDRESS_MAP.get(rpcServiceName);
+        }
+        List<String> res = null;
+        String servicePath = ZK_REGISTER_ROOT_PATH + "/" + rpcServiceName;
+        try {
+            res = zkClient.getChildren().forPath(servicePath);
+            SERVICE_ADDRESS_MAP.putIfAbsent(rpcServiceName, res);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 }
